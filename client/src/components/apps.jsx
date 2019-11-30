a23a9f38d6fc4297a9f1b60c96c46fb3e18f963a
Leo Yeung <leoyeung99@gmail.com>
2019-11-30 14:52:26 -0800

    Finished setting up the folders

---
+++ client/src/components/apps.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+    
+    }
+  
+    render() {
+      return (
+        <div>
+          <h1>Hello, world!</h1>
+    
+        </div>
+      );
+    }
+}
+
+export default App
---
+++ schema.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE if not exists lemonloft;
+
+use lemonloft;
+
+CREATE TABLE listings (
+    listing_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    description VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE listing_images (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    image_url VARCHAR(200) NOT NULL,
+    image_info VARCHAR(100),
+    listing_id int NOT NULL,
+    CONSTRAINT `fk_listing_image` FOREIGN KEY (listing_id)
+    REFERENCES listings(listing_id) ON DELETE CASCADE
+);
