cf377f8c1d7c2a963d1ab03c80e9fcca7332a051
Leo Yeung <leoyeung99@gmail.com>
2019-12-05 10:45:05 -0800

    Added more Jest Test for my component

---
+++ client/src/components/SampleData.js
@@ -0,0 +1,66 @@
+const SampleData = {
+
+    data: [{
+            "id": 31,
+            "image_url": "https://lemonloft-gallery.s3-us-west-1.amazonaws.com/images/img49.jpg",
+            "image_info": "Handcrafted loft",
+            "listing_id": 1
+        },
+        {
+            "id": 32,
+            "image_url": "https://lemonloft-gallery.s3-us-west-1.amazonaws.com/images/img67.jpg",
+            "image_info": "Intelligent loft",
+            "listing_id": 1
+        },
+        {
+            "id": 33,
+            "image_url": "https://lemonloft-gallery.s3-us-west-1.amazonaws.com/images/img69.jpg",
+            "image_info": "Sleek area",
+            "listing_id": 1
+        },
+        {
+            "id": 34,
+            "image_url": "https://lemonloft-gallery.s3-us-west-1.amazonaws.com/images/img51.jpg",
+            "image_info": "Rustic room",
+            "listing_id": 1
+        },
+        {
+            "id": 35,
+            "image_url": "https://lemonloft-gallery.s3-us-west-1.amazonaws.com/images/img34.jpg",
+            "image_info": "Handmade area",
+            "listing_id": 1
+        },
+        {
+            "id": 36,
+            "image_url": "https://lemonloft-gallery.s3-us-west-1.amazonaws.com/images/img46.jpg",
+            "image_info": "Handmade space",
+            "listing_id": 1
+        },
+        {
+            "id": 37,
+            "image_url": "https://lemonloft-gallery.s3-us-west-1.amazonaws.com/images/img17.jpg",
+            "image_info": "Licensed area",
+            "listing_id": 1
+        },
+        {
+            "id": 38,
+            "image_url": "https://lemonloft-gallery.s3-us-west-1.amazonaws.com/images/img43.jpg",
+            "image_info": "Rustic space",
+            "listing_id": 1
+        },
+        {
+            "id": 39,
+            "image_url": "https://lemonloft-gallery.s3-us-west-1.amazonaws.com/images/img34.jpg",
+            "image_info": "Handcrafted space",
+            "listing_id": 1
+        },
+        {
+            "id": 40,
+            "image_url": "https://lemonloft-gallery.s3-us-west-1.amazonaws.com/images/img21.jpg",
+            "image_info": "Unbranded loft",
+            "listing_id": 1
+        }
+    ]
+}
+
+export default SampleData
--- test/Gallery.test.jsx
+++ test/Gallery.test.jsx
@@ -2,16 +2,36 @@ import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
 import Enzyme, { shallow } from 'enzyme';
 import Gallery from '../client/src/components/Gallery.jsx';
+import SampleData from '../client/src/components/SampleData.js';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('Gallery component', () => {
   
-  
-    it('should render the PhotoGrid component', () => {
-    const wrapper = shallow(<Gallery  />);
+    const { data } = SampleData; 
+    const wrapper = shallow(<Gallery pictures={data} />);
+
+    it('should render the Gallery component', () => {
       expect(wrapper.exists()).toBe(true);
     });
-  
+
+    it('should render one photo in the Main Image Section', () => {
+        expect(wrapper.find('.mainImage')).toHaveLength(1);
+    });
+
+    it('should render one photo in the Second Image Section', () => {
+        expect(wrapper.find('.secondImage')).toHaveLength(1);
+    });
+
+    it('should render one photo in the Third Image Section', () => {
+        expect(wrapper.find('.thirdImage')).toHaveLength(1);
+    });
    
-  });
+    it('should render one photo in the Fourth Image Section', () => {
+        expect(wrapper.find('.fourthImage')).toHaveLength(1);
+    });
+
+    it('should render one photo in the Fifth Image Section', () => {
+        expect(wrapper.find('.fifthImage')).toHaveLength(1);
+    });
+});
