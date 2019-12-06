057f48ad578f71eedb4c45b8dc1e8b3620016319
Leo Yeung <leoyeung99@gmail.com>
2019-12-05 21:34:27 -0800

    Lightbox is working but has bugs

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -1,23 +1,17 @@
 import React from 'react'
 import styles from './css/apps.css'
+import Lightbox from './Lightbox.jsx';
 
 class Gallery extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            slideIndex: 1
+        };
+        this.changeSlide = this.changeSlide.bind(this);
+        this.toSlide = this.toSlide.bind(this);
+        this.showSlide = this.showSlide.bind(this);
     }
-
-    // darken (e) {
-
-    //     console.log(e.target.className)
-
-
-        // array = ['mainImage', 'secondImage', 'thirdImage', 'fourthImage', 'fifthImage']
-        // for ( var i = 0; i < array.length;i++){
-        //     if (e.target.value !== array[i]){
-        //         $(`".${e.target.value}"`).addClass("darken")
-        //     }
-        // }
-    // }
 
     darken () {
         $(".secondImage, .thirdImage, .fourthImage, .fifthImage").addClass(`${styles.darken}`);
@@ -53,6 +47,67 @@ class Gallery extends React.Component {
     brighten4 (){
         $(".secondImage, .thirdImage, .fourthImage, .mainImage").removeClass(`${styles.darken}`);
     }
+    // Lightbox Function
+    openLightBox() {
+        $("#LightBox").css("display", "block");
+    }
+
+    closeLightBox() {
+        $("#LightBox").css("display", "none");
+    }
+
+    changeSlide(n){
+        console.log(n)
+        this.setState(prevState => {
+            return {slideIndex: prevState.slideIndex + n}
+         })
+        this.showSlide(this.state.slideIndex + n)
+    }
+
+    toSlide (n) {
+        console.log(n)
+        this.setState({
+            slideIndex: n
+        })
+        this.showSlide(n)
+    }
+
+    onClickHandle(n) {
+        //console.log(n)
+        this.openLightBox();
+        this.toSlide(n);
+    }
+
+    showSlide(n) {
+        const slides = $(".slide");
+        let modalPreviews = $(".modalPreview");
+        console.log(modalPreviews)
+
+        if ( n > slides.length) {
+            this.setState({
+                slideIndex: 1
+            })
+        }
+
+        if ( n < 1 ) {
+            this.setState({
+                slideIndex: slides.length
+            })
+        }
+
+        for ( let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none"
+        }
+
+        for ( let i = 0; i < modalPreviews. length; i++) {
+            modalPreviews[i].className = modalPreviews[i].className.replace(" active", "");
+        }
+
+        slides[this.state.slideIndex-1].style.display = "block";
+
+        modalPreviews[this.state.slideIndex - 1].className += " active";
+
+    }
 
     render() {
 
@@ -60,31 +115,37 @@ class Gallery extends React.Component {
             return (
 
                 <div className={styles.gallery}>
-                    <div onMouseOver={this.darken} onMouseLeave={this.brighten} className={`${styles.item} ${styles.mainImage} mainImage`}>
-                        <a href={this.props.pictures[0].image_url} title={this.props.pictures[0].image_info}>
+                    <div onClick={()=>{this.onClickHandle(1)}} onMouseOver={this.darken} onMouseLeave={this.brighten} className={`${styles.item} ${styles.mainImage} mainImage`}>
+                
                             <img src={this.props.pictures[0].image_url} alt={this.props.pictures[0].image_info} />
-                        </a>
+                        
                     </div>
-                    <div onMouseEnter={this.darken1} onMouseLeave={this.brighten1} className={`${styles.item} ${styles.secondImage} secondImage`}>
-                        <a href={this.props.pictures[1].image_url} title={this.props.pictures[1].image_info}>
+                    <div onClick={()=>{this.onClickHandle(2)}} onMouseEnter={this.darken1} onMouseLeave={this.brighten1} className={`${styles.item} ${styles.secondImage} secondImage`}>
+                  
                             <img src={this.props.pictures[1].image_url} alt={this.props.pictures[1].image_info} />
-                        </a>
+                    
                     </div>
-                    <div onMouseEnter={this.darken2} onMouseLeave={this.brighten2} className={`${styles.item} ${styles.thirdImage} thirdImage`}>
-                        <a href={this.props.pictures[2].image_url} title={this.props.pictures[2].image_info}>
+                    <div onClick={()=>{this.onClickHandle(3)}} onMouseEnter={this.darken2} onMouseLeave={this.brighten2} className={`${styles.item} ${styles.thirdImage} thirdImage`}>
+                     
                             <img src={this.props.pictures[2].image_url} alt={this.props.pictures[2].image_info} />
-                        </a>
+                     
                     </div>
-                    <div onMouseEnter={this.darken3} onMouseLeave={this.brighten3} className={`${styles.item} ${styles.fourthImage} fourthImage`}>
-                        <a href={this.props.pictures[3].image_url} title={this.props.pictures[3].image_info}>
+                    <div onClick={()=>{this.onClickHandle(4)}} onMouseEnter={this.darken3} onMouseLeave={this.brighten3} className={`${styles.item} ${styles.fourthImage} fourthImage`}>
+                    
                             <img src={this.props.pictures[3].image_url} alt={this.props.pictures[3].image_info} />
-                        </a>
+                    
                     </div>
-                    <div onMouseEnter={this.darken4} onMouseLeave={this.brighten4} className={`${styles.item} ${styles.fifthImage} fifthImage`}>
-                        <a href={this.props.pictures[4].image_url} title={this.props.pictures[4].image_info}>
+                    <div onClick={()=>{this.onClickHandle(5)}} onMouseEnter={this.darken4} onMouseLeave={this.brighten4} className={`${styles.item} ${styles.fifthImage} fifthImage`}>
+                      
                             <img src={this.props.pictures[4].image_url} alt={this.props.pictures[4].image_info} />
-                        </a>
+                      
                     </div>
+                    <Lightbox
+                    pictures={this.props.pictures}
+                    closeLightBox={this.closeLightBox}
+                    changeSlide={this.changeSlide}
+                    toSlide={this.toSlide}
+                    />
                 </div>
 
             )
---
+++ client/src/components/Lightbox.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import styles from './css/apps.css'
+
+class Lightbox extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div id="LightBox" className={`${styles.modal} modal`} >
+
+                <span className={`${styles.close} close pointer`} onClick={this.props.closeLightBox}>&times;</span>
+            
+            <div className={`${styles.modalContent} modalContent`}>
+
+                <div className={`${styles.slide} slide`}>
+
+                <img className={`${styles.imageSlide}`} src={this.props.pictures[0].image_url} alt={this.props.pictures[0].image_info} />
+                </div>
+
+                <div className={`${styles.slide} slide`}>
+
+                <img className={`${styles.imageSlide}`} src={this.props.pictures[1].image_url} alt={this.props.pictures[1].image_info} />
+                </div>
+
+                <div className={`${styles.slide} slide`}>
+                <img className={`${styles.imageSlide}`} src={this.props.pictures[2].image_url} alt={this.props.pictures[2].image_info} />
+                </div>
+
+                <a className={`${styles.previous} previous`} onClick={()=>{this.props.changeSlide(-1)}}>&#10094;</a>
+
+                <a className={`${styles.next} next`} onClick={()=>{this.props.changeSlide(1)}}>&#10095;</a>
+
+                <div className={`${styles.dots} dots`}>
+                    <div className={`${styles.col} col`}>
+
+                    <img src={this.props.pictures[0].image_url} className={`${styles.modalPreview} ${styles.hoverShadow}modalPreview hoverShadow`} onClick={()=>{this.props.toSlide(1)}} alt={this.props.pictures[0].image_info} />
+
+                    </div>
+
+                    <div className={`${styles.col} col`}>
+
+                    <img src={this.props.pictures[1].image_url} className={`${styles.modalPreview} ${styles.hoverShadow}modalPreview hoverShadow`} onClick={()=>{this.props.toSlide(2)}} alt={this.props.pictures[1].image_info} />
+
+                    </div>
+
+                    <div className={`${styles.col} col`}>
+                    <img src={this.props.pictures[2].image_url} className={`${styles.modalPreview} ${styles.hoverShadow}modalPreview hoverShadow`}onClick={()=>{this.props.toSlide(3)}} alt={this.props.pictures[2].image_info} />
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+
+        )
+    }
+}
+
+export default Lightbox
